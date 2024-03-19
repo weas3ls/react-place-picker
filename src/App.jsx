a985bca1ded7343858a971eb2af22b22aca10b1a
weas3ls <shabaneh.ahmad@gmail.com>
2024-03-19 15:51:28 -0700

    implement effect to sort places by user's distance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+import logoImg from "./assets/logo.png";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import Modal from "./components/Modal.jsx";
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
-import Modal from "./components/Modal.jsx";
-import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
-import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
     const modal = useRef();
     const selectedPlace = useRef();
+    const [availablePlaces, setAvailablePlaces] = useState([]);
     const [pickedPlaces, setPickedPlaces] = useState([]);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            const sortedPlaces = sortPlacesByDistance(
+                AVAILABLE_PLACES,
+                position.coords.latitude,
+                position.coords.longitude,
+            );
+
+            setAvailablePlaces(sortedPlaces);
+        });
+    }, []);
 
     function handleStartRemovePlace(id) {
         modal.current.open();
@@ -53,7 +67,12 @@ function App() {
                     places={pickedPlaces}
                     onSelectPlace={handleStartRemovePlace}
                 />
-                <Places title="Available Places" places={AVAILABLE_PLACES} onSelectPlace={handleSelectPlace} />
+                <Places
+                    title="Available Places"
+                    places={availablePlaces}
+                    fallbackText={"Sorting places by distance..."}
+                    onSelectPlace={handleSelectPlace}
+                />
             </main>
         </>
     );
